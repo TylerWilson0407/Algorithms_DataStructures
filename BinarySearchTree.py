2f549a545558867e4ed745917ac739f0e95ab06f
wilsont1988 <wilsont1988@gmail.com>
2016-09-30 00:11:10 -0700

    Binary search tree

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -60,6 +60,61 @@ class BinarySearchTree:
         else
             return False
 
+    def delete(self, key):
+        if self.size > 1:
+            nodeToRemove = self._get(key, self.root)
+            if nodeToRemove:
+                self.remove(nodeToRemove)
+                self.size = self.size - 1
+            else:
+                raise KeyError('Error, key not in tree')
+        elif self.size == 1 and self.root.key == key:
+            self.root = None
+            self.size = self.size - 1
+        else:
+            raise KeyError('Error, key not in tree')
+
+    def __delitem__(self, key):
+        self.delete(key)
+
+    def remove(self,node):
+        if node.isLeaf():
+            if node == node.isLeftChild:
+                node.parent.leftChild = None
+            else:
+                node.parent.rightChild = None
+        elif not node.hasBothChildren:
+            if node.hasLeftChild:
+                if node.isLeftChild:
+                    node.leftChild.parent = node.parent
+                    node.parent.leftChild = node.leftChild
+                elif node.isRightChild:
+                    node.rightChild.parent = node.parent
+                    node.parent.rightChild = node.leftChild
+                else:
+                    node.replaceNodeData(node.leftChild.key,
+                                    node.leftChild.payload,
+                                    node.leftChild.leftChild,
+                                    node.leftChild.rightChild)
+            else:
+                if node.isLeftChild:
+                    node.leftChild.parent = node.parent
+                    node.parent.leftChild = node.rightChild
+                elif node.isRightChild:
+                    node.rightChild.parent = node.parent
+                    node.parent.rightChild = node.rightChild
+                else:
+                    node.replaceNodeData(node.rightChild.key,
+                                         node.rightChild.payload,
+                                         node.rightChild.leftChild,
+                                         node.rightChild.rightChild)
+        else:
+            succ = node.findSuccessor()
+            succ.spliceOut()
+            node.key = succ.key
+            node.payload = succ.payload
+
+
 
 
 class TreeNode:
@@ -103,3 +158,52 @@ class TreeNode:
         if self.hasRightChild():
             self.rightChild.parent = self
 
+    def findSuccessor(self):
+        succ = None
+        if self.hasRightChild():
+            succ = self.rightChild.findMin()
+        else:
+            if self.parent:
+                if self.isLeftChild():
+                    succ = self.parent
+                else:
+                    self.parent.rightChild = None
+                    succ = self.parent.findSuccessor()
+                    self.parent.rightChild = self
+        return succ
+
+    def findMin(self):
+        current = self
+        while current.hasLeftChild():
+            current = current.leftChild
+        return current
+
+    def spliceOut(self):
+        if self.isLeaf():
+            if self.isLeftChild():
+                self.parent.leftChild = None
+            else:
+                self.parent.rightChild = None
+        elif self.hasAnyChildren():
+            if self.hasLeftChild():
+                if self.isLeftChild():
+                    self.parent.leftChild = self.leftChild
+                else:
+                    self.parent.rightChild = self.leftChild
+                self.leftChild.parent = self.parent
+            else:
+                if self.isLeftChild():
+                    self.parent.leftChild = self.rightChild
+                else:
+                    self.parent.rightChild = self.rightChild
+                self.rightChild.parent = self.parent
+
+    def __iter__(self):
+        if self:
+            if self.hasLeftChild():
+                for elem in self.leftChild:
+                    yield elem
+            yield self.key
+            if self.hasRightChild():
+                for elem in self.rightChild:
+                    yield elem

1f9b4e5b64abe989837559b3ede37a212323b025
Tyler <wilsont1988@gmail.com>
2016-10-02 19:10:45 -0700

    Continuing Algorithms and Data Structures lessons

--- DepthFirstSearch.py
+++ DepthFirstSearch.py
@@ -14,7 +14,31 @@ class DFSGraph(Graph):
                 self.dfsvisit(aVertex)
 
     def dfsByFinish(self):
+        for aVertex in self:
+            aVertex.setColor('white')
+            aVertex.setPred(-1)
+        for aVertex in self.sortByFinish():
+            if aVertex.getColor() == 'white':
+                self.dfsvisit(aVertex)
 
+    # def dfsvisitByFinish(self, startVertex):
+    #     startVertex.setColor('gray')
+    #     self.time += 1
+    #     startVertex.setDiscovery(self.time)
+    #     for nextVertex in startVertex.getConnections():
+    #         if nextVertex.getColor() == 'white':
+    #             nextVertex.setPred(startVertex)
+    #             self.dfsvisit(nextVertex)
+    #     startVertex.setColor('black')
+    #     self.time += 1
+    #     startVertex.setFinish(self.time)
+
+    def sortByFinish(self):
+        finList = []
+        for v in self.getVertices():
+            finList.append((v.getFinish(), v))
+        finList.sort(key=lambda x: x[0], Reverse=True)
+        return [y[1] for y in finList]
 
     def dfsvisit(self, startVertex):
         startVertex.setColor('gray')
@@ -32,6 +56,8 @@ class DFSGraph(Graph):
 
 def stronglyCC(g):
     g.dfs()
+    gt = transposeGraph(g)
+    gt.dfsByFinish()
 
 
 def transposeGraph(g):
@@ -47,3 +73,19 @@ def buildSampleGraph():
     sg = Graph()
     for letter in letters:
         sg.addVertex(letter)
+    sg.addEdge('a', 'b')
+    sg.addEdge('b', 'c')
+    sg.addEdge('b', 'e')
+    sg.addEdge('c', 'c')
+    sg.addEdge('c', 'f')
+    sg.addEdge('d', 'b')
+    sg.addEdge('d', 'g')
+    sg.addEdge('e', 'a')
+    sg.addEdge('e', 'd')
+    sg.addEdge('f', 'h')
+    sg.addEdge('g', 'e')
+    sg.addEdge('h', 'i')
+    sg.addEdge('i', 'f')
+    return sg
+
+sg = buildSampleGraph()
